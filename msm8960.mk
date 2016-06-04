1c4577e727448ef0e26495a9ece53e428d5b6da2
Matt Mower <mowerm@gmail.com>
2016-06-03 20:28:55 -0500

    msm8960-common: hostapd: Support accept/deny mac lists
    Change-Id: I1f66d29a8272fdda60775f232e1c5cb77fe20423

--- msm8960.mk
+++ msm8960.mk
@@ -93,6 +93,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     dhcpcd.conf \
     hostapd \
+    hostapd.accept \
+    hostapd.deny \
     hostapd_default.conf \
     wpa_supplicant \
     wpa_supplicant.conf
